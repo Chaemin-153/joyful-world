b530fa957a9dcbcc9f2ce451384edd5af57a04ae
Chaemin-153 <0520032@gmail.com>
2025-09-03 16:25:35 +0800

    feat: Product 페이지 추가(미완)

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import PartnerPage from './pages/PartnerPage';
 import BrandStoryPage from './pages/BrandStoryPage';
 import AboutUsPage from './pages/AboutUsPage';
+import ProductPage from './pages/ProductPage';
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -18,7 +19,7 @@ const AppRouter = () => (
         {/* Brand Story 페이지 */}
         <Route path="/brand-story" element={<BrandStoryPage />} />
         {/* Product 페이지 */}
-        {/* <Route path="/product" element={<ProductPage />} /> */}
+        <Route path="/product" element={<ProductPage />} />
         {/* Community 페이지 */}
         {/* <Route path="/community" element={<CommunityPage />} /> */}
       </Route>
