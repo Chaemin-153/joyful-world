0d69642154515b0c41630a59d3754ea8b93d09c6
Chaemin-153 <0520032@gmail.com>
2025-09-19 22:42:34 +0800

    refactor: Footer 모바일 반응형 적용

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,19 +3,12 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer className="w-full border-t bg-gray-50">
-      <div className="mx-auto max-w-6xl px-4 py-6 text-sm text-brown flex justify-between">
+      <div className="flex flex-col md:flex-row justify-between gap-6 mx-auto max-w-6xl px-4 py-6 text-sm text-brown ">
         <Link to="/" className="font-bold text-lg">
-          <img src="/logo_nobackground.png" alt="brand_logo" className="w-36" />
+          <img src="/logo_background.png" alt="brand_logo" className="w-36" />
         </Link>
+
         <div className="flex flex-col gap-2">
-          <div>고객센터 0507-1404-6835</div>
-          <div>월-금 10:00 ~ 16:00</div>
-          <div>점심시간 12:00 ~ 13:00</div>
-          <div>주말 및 공휴일 휴무</div>
-        </div>
-        <div className="flex flex-col gap-2">
-          {/* <div>joyful_honeyjam@naver.com</div>
-          <div>INSTAGRAM</div> */}
           <div className="flex items-center gap-2">
             <img
               src="/fav-email.png"
@@ -25,7 +18,7 @@ const Footer = () => {
             <span>joyful_honeyjam@naver.com</span>
           </div>
           <a
-            href="https://instagram.com"
+            href="https://www.instagram.com/joyful_jjam/"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center gap-2"
@@ -39,11 +32,17 @@ const Footer = () => {
           </a>
         </div>
         <div className="flex flex-col gap-2">
+          <div>고객센터 0507-1404-6835</div>
+          <div>월-금 10:00 ~ 16:00</div>
+          <div>점심시간 12:00 ~ 13:00</div>
+          <div>주말 및 공휴일 휴무</div>
+        </div>
+        <div className="flex flex-col gap-2">
           <Link to="/about-us" className="underline">
             About Us
           </Link>
-          <Link to="/partner" className="underline">
-            Partner
+          <Link to="/contact" className="underline">
+            Contact
           </Link>
           <Link to="/brand-story" className="underline">
             Brand Story
@@ -58,11 +57,17 @@ const Footer = () => {
       </div>
       <div className="mx-auto max-w-6xl px-4 py-6 text-sm text-brown flex flex-col gap-2">
         <span>
-          잼있는 세상 | 사업자등록번호 721-34-01365 | 통신판매업신고번호 제
-          2024-서울광진-1525 | 대표 이지은
+          잼있는 세상 | 사업자등록번호 721-34-01365{' '}
+          <br className="block md:hidden" />
+          <span className="hidden md:inline">| </span> 통신판매업신고번호 제
+          2024-서울광진-1525 <span className="hidden md:inline">| </span>{' '}
+          <br className="block md:hidden" />
+          대표 이지은
         </span>
         <span>
-          주소 | 서울특별시 광진구 아차산로 294 4층 청년쿡비즈니스센터 |
+          주소 | 서울특별시 광진구 아차산로 294 4층 청년쿡비즈니스센터{' '}
+          <span className="hidden md:inline">| </span>
+          <br className="block md:hidden" />
           COPYRIGHT© 잼있는 세상 ALL RIGHT RESERVED.
         </span>
       </div>
