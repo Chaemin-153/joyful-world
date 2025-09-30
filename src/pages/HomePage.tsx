d22b463c5c4e5387a770254d37ed5bfa3dfd586d
Chaemin-153 <0520032@gmail.com>
2025-09-30 19:32:43 +0800

    feat: HomePage에 LanguageToggle 기능 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
 import ContactPage from './ContactPage';
 import AboutUsPage from './AboutUsPage';
+import { useTranslation } from 'react-i18next';
 
 const HomePage = () => {
+  const { t } = useTranslation('common');
+
   return (
     <div className="flex flex-col items-center gap-8 text-brown text-center">
       <AboutUsPage />
@@ -25,13 +28,13 @@ const HomePage = () => {
                 className="w-full h-64 object-cover rounded-lg"
               />
               <div className="absolute inset-0 bg-black/40 rounded-lg flex flex-col gap-6 items-start justify-start px-8 py-14">
-                <h4 className="text-2xl font-bold">인생꿀잼(Joyful Jam)</h4>
-                <p>맛있고 즐거운 디저트로 특별한 미식 경험을 제공합니다</p>
+                <h4 className="text-2xl font-bold">{t('home.box1.title')}</h4>
+                <p>{t('home.box1.desc')}</p>
                 <Link
-                  to={'/product'}
+                  to={'/products'}
                   className="bg-yellow text-black font-bold text:md md:text-lg rounded-sm px-6 md:px-8 py-3"
                 >
-                  제품 바로가기
+                  {t('home.link')}
                 </Link>
               </div>
             </div>
@@ -43,13 +46,13 @@ const HomePage = () => {
                 className="w-full h-64 object-cover rounded-lg"
               />
               <div className="absolute inset-0 bg-black/40 rounded-lg flex flex-col gap-6 items-start justify-start px-8 py-14">
-                <h4 className="text-2xl font-bold">와이즈(WISE)</h4>
-                <p>좋은 원재료와 영양성분으로 똑똑한 식습관을 도와줍니다</p>
+                <h4 className="text-2xl font-bold">{t('home.box2.title')}</h4>
+                <p>{t('home.box2.desc')}</p>
                 <Link
-                  to={'/product'}
-                  className="bg-yellow text-black font-bold text:md md:ttext-lg rounded-sm px-6 md:px-8 py-3"
+                  to={'/products'}
+                  className="bg-yellow text-black font-bold text:md md:text-lg rounded-sm px-6 md:px-8 py-3"
                 >
-                  제품 바로가기
+                  {t('home.link')}
                 </Link>
               </div>
             </div>
